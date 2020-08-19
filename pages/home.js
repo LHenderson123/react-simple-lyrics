0c9f3c533fe5044d4881ce07afa8ccf4e6ce88bc
LHenderson123 <1014185@student.ccgs.wa.edu.au>
2020-08-19 13:25:36 +0800

    Initial commit

---
+++ pages/home.js
@@ -0,0 +1,39 @@
+import React, { useState} from 'react'
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography"
+import TextField from "@material-ui/core/TextField"
+import {Redirect} from "react-router-dom";
+
+export default function App() {
+  const [song, setSong] = useState("");
+  const [artist, setArtist] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  if (redirect) {
+    return <Redirect push to={`/${artist}/${song}`} />
+  }
+  return (
+    <>
+      <TextField
+        id="outlined-basic"
+        label="Song"
+        variant="outlined"
+        value={song}
+        onChange={event => setSong(event.target.value)}
+      />
+
+      <TextField
+        id="outlined-basic"
+        label="Artist"
+        variant="outlined"
+        value={artist}
+        onChange={event => setArtist(event.target.value)}
+      />
+
+      <Button variant="contained" onClick={() => setRedirect(true) }>
+        Search
+      </Button>
+    </>
+  );
+}
+
---
+++ pages/lyrics.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import useFetch from '../hooks/useFetch'
+import Typography from '@material-ui/core/Typography'
+
+import { useParams } from "react-router-dom";
+
+export default function LyricPage(){
+  
+  let { song, artist } = useParams();
+
+  return <Lyrics song={song} artist={artist} />
+}
+
+function Lyrics({ song, artist }) {
+  let api = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+
+  const { data, loading, error } = useFetch(api);
+
+  if (loading) {
+    return <Typography variant="p">loading...</Typography>;
+  }
+  if (error) {
+    return <Typography variant="p">{error}</Typography>;
+  }
+  return (
+    <>
+      <Typography variant="h3">
+        {song} by {artist} lyrics
+      </Typography>
+      <Typography variant ="p">{data}</Typography>
+    </>
+  );
+}
+
